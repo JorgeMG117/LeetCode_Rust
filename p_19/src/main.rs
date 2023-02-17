9b700dbf539615046a51138a8f597c5cfa828431
JorgeMG117 <jorgemartinezgil117@gmail.com>
2023-02-17 22:01:31 +0100

    Problem 19 working

--- p_19/src/main.rs
+++ p_19/src/main.rs
@@ -8,33 +8,24 @@ pub struct ListNode {
 }
 
 impl ListNode {
-    #[inline]
-   fn new(val: i32) -> Self {
-        ListNode {
-            next: None,
-            val
-        }
-    }
+//    #[inline]
+//    fn new(val: i32) -> Self {
+//         ListNode {
+//             next: None,
+//             val
+//         }
+//     }
 }
 
 impl Solution {
-    fn remove_nth_from_end_req(head: Option<Box<ListNode>>, n: i32) -> i32 {
-        //si es None
-            //devolvemos 1
-        //sino
-            //val = f(next)
-            //si val = n //hay que eliminar este nodo
-                //apuntamos ha este nodo
-                //con el que nos han llamado es igual a esteNodo.next
-            //sino return val+1
+    fn remove_nth_from_end_req(head: &mut Option<Box<ListNode>>, n: i32) -> i32 {
         if let Some(node) = head {
-            let val = Self::remove_nth_from_end_req(node.next, n);
+            let val = Self::remove_nth_from_end_req(&mut node.next, n);
             if val == n {
                 let nodo_val = node.val;
                 println!("Eliminar nodo {nodo_val}");
 
-                let aux = node;
-                node.next = aux.next;
+                *head = head.as_mut().unwrap().next.take();
             }
             return val + 1;
         } else {
@@ -43,57 +34,27 @@ impl Solution {
     }
 
     pub fn remove_nth_from_end(head: Option<Box<ListNode>>, n: i32) -> Option<Box<ListNode>> {
-        //let list = ListNode::new(3);
-        //let mut value = list.val;
-        //println!("{value}");
-        
-        /*if let Some(v) = head {
-            let value = v.val;
-            println!("{value}");
-        }*/
-
-
-        /* 
-        //add additional node to handle case of head removal
-        let mut dummy = Some(Box::new(ListNode {
-            val: -1,
-            next: head,
-        }));
-
-        //calculate length of the list
-        let mut len = 0;
-        let mut temp = dummy.as_ref();
-        while temp.is_some() {
-            temp = temp.unwrap().next.as_ref();
-            len += 1;
-        }
-
-        //calculate the index to be removed
-        let index = len - n - 1;
-
-        //get to the node before the index
-        let mut dummy_mut = dummy.as_mut().unwrap();
-        for _ in 0..index {
-            dummy_mut = dummy_mut.next.as_mut().unwrap();
-        }
-
-        //remove the node
-        let next_node = dummy_mut.next.take();
-        if let Some(node_to_remove) = next_node {
-            dummy_mut.next = node_to_remove.next;
-        }
-
-        return dummy.unwrap().next;
-        */
-
-
-        Self::remove_nth_from_end_req(head, 32);
+        let mut head = head;
+        Self::remove_nth_from_end_req(&mut head, n);
         return head;
-
     }
 }
 
 // 19. Remove Nth Node From End of List
 fn main() {
-    println!("Hello, world!");
+    //let mut head = Some(Box::new(ListNode { val: 1, next: None }));
+    //let mut head = Some(Box::new(ListNode { val: 1, next: Some(Box::new(ListNode { val: 2, next: None })) }));
+    //let mut head = Some(Box::new(ListNode { val: 1, next: Some(Box::new(ListNode { val: 2, next: Some(Box::new(ListNode { val: 3, next: None })) })) }));
+    let head = Some(Box::new(ListNode { val: 1, next: Some(Box::new(ListNode { val: 2, next: Some(Box::new(ListNode { val: 3, next: Some(Box::new(ListNode { val: 4, next: None })) })) })) }));
+    let n = 2;
+    
+    let sol = Solution::remove_nth_from_end(head, n);
+
+    //Print the elements of the list
+    let mut curr = sol;
+    while let Some(node) = curr {
+        let value = node.val;
+        println!("{value}");
+        curr = node.next;
+    }
 }
