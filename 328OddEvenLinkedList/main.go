e52ab2e8b8c67f154a06f76ae9ea1f5a9c6dd5e3
JorgeMG117 <jorgemartinezgil117@gmail.com>
2024-06-16 21:59:59 +0200

    328 done

--- 328OddEvenLinkedList/main.go
+++ 328OddEvenLinkedList/main.go
@@ -4,69 +4,92 @@ import "fmt"
 
 // Definition for singly-linked list.
 type ListNode struct {
-    Val int
-    Next *ListNode
+	Val  int
+	Next *ListNode
 }
 
 func oddEvenList(head *ListNode) *ListNode {
-    firstOdd := &head
-    firstEven := head.Next
+	var firstOdd *ListNode
+	var firstEven *ListNode
+	isOdd := true
 
-    odd := firstOdd
+	var nextOdd **ListNode
+	var nextEven **ListNode
 
-    for odd != nil {
+	current := head
 
-    }
-    /*
-    iter := head
-    odd := true
+	for current != nil {
+		if isOdd {
+			//fmt.Println("Val is odd ", current.Val)
+			isOdd = false
 
-    var changeOdd *ListNode
-    var changeEven *ListNode
+			if firstOdd == nil {
+				firstOdd = current
+				nextOdd = &current.Next
+			} else {
+				*nextOdd = current
+				nextOdd = &current.Next
+			}
 
-    for iter != nil {
-        if odd {
-            changeOdd = iter
-        } else {
-            nextEven
-        }
-    }
+		} else {
+			//fmt.Println("Val is even ", current.Val)
+			if firstEven == nil {
+				firstEven = current
+				nextEven = &current.Next
+			} else {
+				*nextEven = current
+				nextEven = &current.Next
+			}
 
-    odd := addEventList(iter.Ne
-    */
-    
-    return head
+			isOdd = true
+		}
+
+		next := current.Next
+		current.Next = nil
+		current = next
+
+	}
+
+	if nextOdd != nil {
+		*nextOdd = firstEven
+	}
+
+	//fmt.Println((**nextOdd).Val)
+	//fmt.Println((*firstEven).Val)
+	//fmt.Println((*firstOdd).Val)
+
+	return head
 }
 
 func printLinkedList(head *ListNode) {
-    iter := head
-    for iter != nil {
-        fmt.Print(iter.Val)
-        fmt.Print(", ")
-        iter = iter.Next
-    }
-    fmt.Println()
+	iter := head
+	for iter != nil {
+		fmt.Print(iter.Val)
+		fmt.Print(", ")
+		iter = iter.Next
+	}
+	fmt.Println()
 }
 
 /*
 Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
 */
 func main() {
-    input := []int{2,1,3,5,6,4,7}
+	input := []int{2, 1, 3, 5, 6, 4, 7}
 
-    var list *ListNode
-    for i := len(input)-1; i >= 0; i-- {
-        v := input[i]
-        node := &ListNode{
-            Val: v,
-            Next: list,
-        }
-        list = node
-    }
+	var list *ListNode
+	for i := len(input) - 1; i >= 0; i-- {
+		v := input[i]
+		node := &ListNode{
+			Val:  v,
+			Next: list,
+		}
+		list = node
+	}
 
-    fmt.Println("Initial list")
-    printLinkedList(list)
-    result := oddEvenList(list)
-    fmt.Println("Computed list")
-    printLinkedList(result)
+	fmt.Println("Initial list")
+	printLinkedList(list)
+	result := oddEvenList(list)
+	fmt.Println("Computed list")
+	printLinkedList(result)
 }
